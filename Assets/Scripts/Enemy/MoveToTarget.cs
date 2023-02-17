71146888980bd12a618239787aac35fc7bad7a5a
Liam Heaney <71699673+rhexn@users.noreply.github.com>
2023-02-17 00:14:32 +0000

    AI running towards player and stopping
    Set AI to change speed once closer to player, also set the AI to gradually stop before they are too close

--- Assets/Scripts/Enemy/MoveToTarget.cs
+++ Assets/Scripts/Enemy/MoveToTarget.cs
@@ -8,6 +8,14 @@ public class MoveToTarget : MonoBehaviour
     public Transform target;
     private NavMeshAgent agent;
 
+    [SerializeField]
+    private float runDist;
+
+    [SerializeField]
+    private float stopDist;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +26,31 @@ public class MoveToTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AIMove();
+    }
+
+    private void AIMove()
+    {
+        float dist = Vector3.Distance(agent.transform.position, target.position);
+
         agent.SetDestination(target.position);
+
+        if(dist < runDist)
+        {
+            agent.speed = 5;
+        }
+        else
+        {
+            agent.speed = 3.5f;
+        }
+
+        if (dist < stopDist)
+        {
+            agent.isStopped = true;
+        }
+        else 
+        {
+            agent.isStopped = false;
+        }
     }
 }
