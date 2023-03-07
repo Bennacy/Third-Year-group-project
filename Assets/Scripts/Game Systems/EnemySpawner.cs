c6fc587d7a9ecde681a384cfee0cc429fc869ca4
Liam Heaney <71699673+rhexn@users.noreply.github.com>
2023-03-07 02:06:39 +0000

    Second Option for Waves
    Made a second option of Waves of enemies, where this time there is only a limited number of enemies per wave, once the enemies are defeated the next wave will start after a small time delay

--- Assets/Scripts/Game Systems/EnemySpawner.cs
+++ Assets/Scripts/Game Systems/EnemySpawner.cs
@@ -4,35 +4,85 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public int numWaves;
-    public float timeBetweenWaves;
-    public int startingEnemiesPerWave;
+
+    
+    [Header("References")]
+    
     public EnemyScriptableObject[] enemyTypes;
     public Transform[] spawnPoints;
+    [Space(10)]
 
 
+    [Header("Enemy")]
+    public int startingEnemiesPerWave;
+    public int totalEnemies = 10;
+    private int enemiesRemaining;
     private int enemiesPerWave;
+    [Space(10)]
+
+    [Header("Waves")]
+    public int numWaves;
+    public int totalWaves = 10;
+    private int currentWave = 1;
+    [Space(10)]
+    
+    [Header("Time")]
+    public int waveDelay;
+    public float spawnDelay;
 
     private void Start()
     {
         enemiesPerWave = startingEnemiesPerWave;
+        enemiesRemaining = totalEnemies;
         StartCoroutine(SpawnWaves());
     }
 
    private IEnumerator SpawnWaves()
     {
-        for (int i = 0; i < numWaves; i++)
+        //for (int i = 0; i < numWaves; i++)
+        //{
+        //    for (int j = 0; j < enemiesPerWave; j++)
+        //    {
+        //        GameObject prefab = SelectEnemyType();
+        //        Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        //        Instantiate(prefab, randomSpawnPoint.position, randomSpawnPoint.rotation);
+
+        //    }
+        //    enemiesPerWave = enemiesPerWave += Random.Range(1, 5);
+        //    yield return new WaitForSeconds(spawnDelay);
+        //}
+
+
+        while (currentWave <= totalWaves)
         {
-            for (int j = 0; j < enemiesPerWave; j++)
+
+
+            while (enemiesRemaining > 0)
             {
                 GameObject prefab = SelectEnemyType();
                 Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
                 Instantiate(prefab, randomSpawnPoint.position, randomSpawnPoint.rotation);
+
+
+                enemiesRemaining--;
+
+                yield return new WaitForSeconds(spawnDelay);
             }
-            enemiesPerWave = enemiesPerWave += Random.Range(1, 5);
-            yield return new WaitForSeconds(timeBetweenWaves);
+
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                currentWave++;
+                totalEnemies = totalEnemies += Random.Range(0, 6);
+                enemiesRemaining = totalEnemies;
+
+            }
+
+            Debug.Log(currentWave);
+            yield return new WaitForSeconds(waveDelay);
         }
-    } 
+    }
+
+   
 
     /// <summary>
     /// Iterates through the different enemy types, adds up the spawn weight of each enemy type and generates a random number between 0 and the total weight.
