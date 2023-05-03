96f44f8e66701a48b8048d440bee6583d9ddc173
Bennacy <benjamimrodrigues.mg@gmail.com>
2023-05-03 07:54:10 +0100

    UI navigation

---
+++ Assets/Scripts/Interfaces/ISelectable.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ISelectable : MonoBehaviour
+{
+    public GameObject firstSelected;
+    public EventSystem eventSystem;
+    
+    void Start()
+    {
+        eventSystem = EventSystem.current;
+
+    }
+
+    void OnEnable()
+    {
+        SetSelected();
+    }
+
+    public void SetSelected(){
+        eventSystem.SetSelectedGameObject(firstSelected);
+    }
+}
